439c81b9fc2a5b5588e0b9d2ced194a7f866392c
talhafaki <talhafaki05@gmail.com>
2022-03-11 16:04:14 +0300

    Initial Commit

---
+++ NextFlix-Swift/NextFlix_SwiftApp.swift
@@ -0,0 +1,20 @@
+//
+//  NextFlix_SwiftApp.swift
+//  NextFlix-Swift
+//
+//  Created by Talha Faki on 11.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct NextFlix_SwiftApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ NextFlix-SwiftUITests/NextFlix_SwiftUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  NextFlix_SwiftUITestsLaunchTests.swift
+//  NextFlix-SwiftUITests
+//
+//  Created by Talha Faki on 11.03.2022.
+//
+
+import XCTest
+
+class NextFlix_SwiftUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
