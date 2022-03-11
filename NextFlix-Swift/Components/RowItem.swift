edc64d82a7f63c2854c2e05239c23b1b6f83c189
talhafaki <talhafaki05@gmail.com>
2022-03-11 21:43:49 +0300

    format code

--- NextFlix-Swift/Components/RowItem.swift
+++ NextFlix-Swift/Components/RowItem.swift
@@ -10,22 +10,22 @@ import SwiftUI
 struct RowItem: View {
     var body: some View {
         VStack {
-                    HStack {
-                        Spacer().frame(width: 16)
-                        Image(systemName: "phone.fill")
-                            .resizable()
-                            .frame(width: 50.0, height: 80.0)
-                            .padding(10.0)
-                            .background(Color.blue)
-                            .clipShape(Circle())
-                        Spacer().frame(width: 25)
-                        VStack {
-                            Text("Hello, First!")
-                            Text("Hello, Second!")
-                        }
-                        Spacer()
-                    }
+            HStack {
+                Spacer().frame(width: 16)
+                Image(systemName: "phone.fill")
+                    .resizable()
+                    .frame(width: 50.0, height: 80.0)
+                    .padding(10.0)
+                    .background(Color.blue)
+                    .clipShape(Circle())
+                Spacer().frame(width: 25)
+                VStack {
+                    Text("Hello, First!")
+                    Text("Hello, Second!")
                 }
+                Spacer()
+            }
+        }
         .frame(maxWidth: .infinity)
         .background(Color.gray)
         .clipShape(RoundedRectangle(cornerRadius:8))
